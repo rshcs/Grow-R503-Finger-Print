1b2df35cf22f0501cf8a6882ae69b9bcfda66558
roshancs <roshan.cs790@gmail.com>
2023-05-20 13:57:10 +0530

    read_prod_info and read_prod_info_decode added. new confirmation codes added.

--- r503.py
+++ r503.py
@@ -1,9 +1,8 @@
 import serial
-from time import sleep
+from time import sleep, time
 from struct import pack, unpack
 import sys
 import json
-
 
 class R503:
     header = pack('>H', 0xEF01)
@@ -24,9 +23,6 @@ class R503:
 
     def ser_close(self):
         self.ser.close()
-
-    def send_msg(self, *args):  # pkgid, pkglen, instcode, pkg
-        return self.b_array(self.header, self.addr, *args) + self.calc_checksum(*args).value.to_bytes(2, 'big')
     
     def read_msg(self, data_stream):
         hdr_rd, adr_rd, pkg_id_rd, pkg_len_rd = unpack('>HIBH', data_stream[:9])
@@ -46,7 +42,6 @@ class R503:
         # cmd = ctrl << 24 | speed << 16 | color << 8 | cycles
         cmd = pack('>BBBB', ctrl, speed, color, cycles)
         rd = self.ser_send(pid=self.pid_command, pkg_len=0x07, instr_code=0x35, pkg=cmd)
-        print(rd[4])
         return rd[4][0]
 
     def read_sys_para(self):
@@ -65,18 +60,18 @@ class R503:
         """
         rsp = self.read_sys_para()
         pkg_length = {0: 32, 1: 64, 2: 128, 3: 256}
-        sys_parameters = {
-            'system_busy': bool(rsp[0] & 8),
-            'matching_finger_found': bool(rsp[0] & 4),
-            'pw_verified': bool(rsp[0] & 2),
-            'valid_image_in_buffer': bool(rsp[0] & 1),
+        return {
+            'system_busy': bool(rsp[0] & 1),
+            'matching_finger_found': bool(rsp[0] & 2),
+            'pw_verified': bool(rsp[0] & 4),
+            'valid_image_in_buffer': bool(rsp[0] & 8),
             'system_id_code': rsp[1],
             'finger_library_size': rsp[2],
             'security_level': rsp[3],
             'device_address': hex(rsp[4]),
             'data_packet_size': pkg_length[rsp[5]],
-            'baud_rate': rsp[6]*9600}
-        return sys_parameters
+            'baud_rate': rsp[6] * 9600,
+        }
 
     def check_pw(self, pw=0x00):
         return self.ser_send(pid=self.pid_command, pkg_len=0x07, instr_code=0x13, pkg=pack('>I', pw))[4][0]
@@ -87,9 +82,9 @@ class R503:
     def check_sensor(self):
         return self.ser_send(pid=self.pid_command, pkg_len=0x03, instr_code=0x36)[4][0]
 
-    def confirmation_decode(self, msg):
-        confirmation_codes = self.conf_codes()
-        return confirmation_codes[str(msg)]
+    def confirmation_decode(self, c_code):
+        cc = self.conf_codes()
+        return cc[str(c_code)] if c_code in cc else 'others: system reserved'
 
     def get_img(self):
         """
@@ -127,6 +122,39 @@ class R503:
         package = pack('>BH', buffer_id, page_id)
         return self.ser_send(pid=self.pid_command, pkg_len=0x06, instr_code=0x06, pkg=package, demo_mode=True)[4][0]
 
+    def empty_finger_lib(self):
+        return self.ser_send(pid=self.pid_command, pkg_len=0x03, instr_code=0x0d)[4][0]
+
+    def read_valid_template_num(self):
+        return self.ser_send(pid=self.pid_command, pkg_len=0x03, instr_code=0x1d)[4][0]
+    # def get_fingerprint(self):
+    #     # self.led_control()
+    #     t1 = time()
+    #     img_buf = self.get_img_ex()
+    #     print('success' if img_buf == 0 else 'fail')
+    #     for k, v in self.read_sys_para_decode().items():
+    #         print(k, ": ", v)
+    #     # self.led_control(ctrl=4)
+
+    def read_prod_info(self):
+        info = self.ser_send(pid=self.pid_command, pkg_len=0x03, instr_code=0x3c)[4]
+        return info[1:17], info[17:21], info[21:29], info[29:31], info[31:39], info[39:41], info[41:43], info[43:45], info[45:47]
+
+    def read_prod_info_decode(self):
+        inf = self.read_prod_info()
+        return {
+            'module type': inf[0].decode('ascii').replace('\x00', ''),
+            'batch number': inf[1].decode('ascii'),
+            'serial number': inf[2].decode('ascii'),
+            'hw main version': inf[3][0],
+            'hw sub version': inf[3][1],
+            'sensor type': inf[4].decode('ascii'),
+            'image width': unpack('>H', inf[5])[0],
+            'image height': unpack('>H', inf[6])[0],
+            'template size': unpack('>H', inf[7])[0],
+            'fp database size': unpack('>H', inf[8])[0]
+        }
+
     def ser_send(self, pid, pkg_len, instr_code, pkg=None, demo_mode=False):
         """
         pid, pkg_len, instr_code, pkg
@@ -136,11 +164,11 @@ class R503:
             send_values += pkg
         check_sum = sum(send_values)
         send_values = self.header + self.addr + send_values + pack('>H', check_sum)
-        print(send_values)
+        print(send_values.hex(sep=' '))
         if not demo_mode:
             self.ser.write(send_values)
             read_val = self.ser.read(128)
-            print(read_val)
+            print(read_val.hex(sep=' '))
             if read_val == b'':
                 sys.exit('Respond not received from the module')
             else:
@@ -150,13 +178,9 @@ class R503:
 if __name__ == '__main__':
     fp = R503(port=5)
 
-    #pid, pkg_len, instruction_code, pkg
 
-    # msg = fp.read_sys_para_decode()
-    # for k, v in msg.items():
-    #     print(k, ": ", v)
-
-    # fp.store(buffer_id=1, page_id=1)
-    # print(fp.conf_codes())
-
+    # for k, v in fp.read_sys_para_decode().items():
+    #     print(k, v)
+    print(fp.read_prod_info_decode())
+    # fp.get_fingerprint()
     fp.ser_close()
